c469a309a303969355275c99ee529422e497c57b
huynhlv <huynhlevan1997@gmail.com>
2019-04-14 22:48:37 +0700

    Basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
+  get "sessions/new"
+
   scope "(:locale)", locale: /en|vi/ do
     root "users#new"
+    get "/signup", to: "users#new"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
     resources :users
   end
 end
