d6f55cc425bd52f0cc9daa232514a0b6fe95cb8d
huynhlv <huynhlevan1997@gmail.com>
2019-05-12 15:48:04 +0700

    Using gem cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < ActionController::Base
   include Admin::AdministratorsHelper
   before_action :set_locale
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.html{redirect_to main_app.root_url, notice: exception.message}
+      format.js{head :forbidden, content_type: "text/html"}
+    end
+  end
+
   private
 
   def set_locale
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  before_action :authenticate_user!
+  authorize_resource
   before_action :load_tour, only: :create
   before_action :load_booking, only: :change_status
 
